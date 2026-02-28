bfcad70228a45446b9c3eb0f3180fd287d7ebf40
ozonelayer-4 <cosmiccaves67@gmail.com>
2026-02-28 10:24:08 -0800

    got our first code running correctly

--- 2026KitBotInline/src/main/java/frc/robot/Robot.java
+++ 2026KitBotInline/src/main/java/frc/robot/Robot.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// btw we're so screwed i have no clue what im doing -Elliot 
 package frc.robot;
 
 import edu.wpi.first.hal.HAL;
@@ -9,7 +9,6 @@ import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 
 /**
  * The VM is configured to automatically run this class, and to call the
